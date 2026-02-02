beb407a18876eb543e901b6350538dfc2aba286f
myranto <my.randrianantoandro@gmail.com>
2026-02-02 20:22:40 +0300

    Update CompanyController.java

--- src/main/java/gendev/it/serenity/users/controller/CompanyController.java
+++ src/main/java/gendev/it/serenity/users/controller/CompanyController.java
@@ -1,6 +1,8 @@
 package gendev.it.serenity.users.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import gendev.it.serenity.common.controller.CommonController;
@@ -9,6 +11,7 @@ import gendev.it.serenity.users.domain.dto.CompanyDTO;
 
 @RestController
 @RequestMapping("api/company")
+@CrossOrigin(methods = { RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT })
 public class CompanyController extends CommonController<CompanyDTO,CompanyService> {
 
     public CompanyController(CompanyService service) {

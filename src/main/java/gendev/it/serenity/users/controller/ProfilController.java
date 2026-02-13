9e94bf4150d432c96b68524dae892e06be0ec681
Eloic Rakoto-Arisoa <eloicmax@gmail.com>
2026-02-13 15:48:27 +0300

    Update ProfilController.java

--- src/main/java/gendev/it/serenity/users/controller/ProfilController.java
+++ src/main/java/gendev/it/serenity/users/controller/ProfilController.java
@@ -6,13 +6,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import gendev.it.serenity.common.controller.CommonController;
 import gendev.it.serenity.users.application.ProfilService;
 import gendev.it.serenity.users.domain.dto.ProfilDTO;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 

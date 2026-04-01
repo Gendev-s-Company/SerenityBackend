c55eb71c2915aa7438fa833e4e8c16787259679e
myranto <my.randrianantoandro@gmail.com>
2026-04-01 21:37:17 +0300

    validation reservation

--- src/main/java/gendev/it/serenity/restaurant/application/tables/TOccupationService.java
+++ src/main/java/gendev/it/serenity/restaurant/application/tables/TOccupationService.java
@@ -3,9 +3,12 @@ package gendev.it.serenity.restaurant.application.tables;
 import org.springframework.stereotype.Service;
 
 import gendev.it.serenity.common.application.CommonService;
+import gendev.it.serenity.hotel.domain.dto.room.reservation.ReservationDTO;
+import gendev.it.serenity.hotel.infrastructure.entity.room.reservation.Reservation;
 import gendev.it.serenity.restaurant.domain.dto.tables.TOccupationDTO;
 import gendev.it.serenity.restaurant.infrastructure.entity.tables.TableOccupation;
 import gendev.it.serenity.restaurant.infrastructure.repository.tables.TOccupationRepo;
+import jakarta.transaction.Transactional;
 
 @Service
 public class TOccupationService extends CommonService<TableOccupation, TOccupationDTO, String, TOccupationRepo>{
@@ -15,4 +18,19 @@ public class TOccupationService extends CommonService<TableOccupation, TOccupati
         //TODO Auto-generated constructor stub
     }
 
+    @Transactional
+    public void updateState(String id, Integer state) throws Exception {
+        if (state == null)
+            throw new Exception("veuillez indiquer le state");
+        TOccupationDTO resa = findById(id, 0);
+        if (resa == null) {
+            throw new Exception("Reservation ID " + id + " introuvable");
+        }
+        TableOccupation toUpdate = resa.dtoToEntity();
+        toUpdate.setState(state);
+        TableOccupation toArchive =  getJpa().save(toUpdate);
+        // toArchive = toArchive.entityToDTO().dtoToEntity();
+        // toArchive.setState(resa.getState());
+        // archivateReservation(toArchive);
+    }
 }
--- src/main/java/gendev/it/serenity/restaurant/controller/tables/TableOccupationController.java
+++ src/main/java/gendev/it/serenity/restaurant/controller/tables/TableOccupationController.java
@@ -1,8 +1,13 @@
 package gendev.it.serenity.restaurant.controller.tables;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import gendev.it.serenity.common.controller.CommonController;
@@ -17,6 +22,18 @@ public class TableOccupationController extends CommonController<TOccupationDTO, 
     public TableOccupationController(TOccupationService service) {
         super(service);
         //TODO Auto-generated constructor stub
+    }
+
+    @PutMapping("/update/state/{id}")
+    public ResponseEntity<?> updateState(@PathVariable String id, @RequestParam(name = "state") Integer state) {
+        try {
+            getService().updateState(id, state);
+            return ResponseEntity.ok("Modification réussi");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+
+        }
     }
 
 }

bdea16cd99e764e285052edbfb82058821ade9ec
Eloic Rakoto-Arisoa <eloicmax@gmail.com>
2026-04-22 09:44:02 +0300

    Error syntax
    Erreur de syntaxe sur les Entités de la disponibilité des tables.

--- src/main/java/gendev/it/serenity/restaurant/domain/dto/tables/TableDsiponibilityDTO.java
+++ src/main/java/gendev/it/serenity/restaurant/domain/dto/tables/TableDsiponibilityDTO.java
@@ -2,7 +2,7 @@ package gendev.it.serenity.restaurant.domain.dto.tables;
 
 public interface TableDsiponibilityDTO {
     public String getTableID();
-    public String getName();
+    public String getTable_name();
     public Integer getTable_state();
     public Integer getReservation_state();
 }

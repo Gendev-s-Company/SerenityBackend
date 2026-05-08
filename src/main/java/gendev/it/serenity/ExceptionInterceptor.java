fb1a6528a7eed8bd22f481414b187eff3398588c
Eloic Rakoto-Arisoa <eloicmax@gmail.com>
2026-05-08 11:19:38 +0300

    Update ExceptionInterceptor.java

--- src/main/java/gendev/it/serenity/ExceptionInterceptor.java
+++ src/main/java/gendev/it/serenity/ExceptionInterceptor.java
@@ -6,15 +6,21 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestControllerAdvice
 public class ExceptionInterceptor {
+
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<String> handleInvalidJson(HttpMessageNotReadableException e) {
 
         Throwable cause = e.getMostSpecificCause();
+
+        log.error("Erreur JSON", cause);
+
         return ResponseEntity
                 .status(HttpStatus.NOT_ACCEPTABLE)
                 .body("Erreur de validation : " + cause.getMessage());
     }
-
 }

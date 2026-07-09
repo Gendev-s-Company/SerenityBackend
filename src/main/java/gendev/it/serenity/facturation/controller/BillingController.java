ef651559ba504a0c4a992a64f2cfef06dbaa34a2
myranto <95678823+myranto@users.noreply.github.com>
2026-07-09 11:11:05 +0300

    next task

--- src/main/java/gendev/it/serenity/facturation/controller/BillingController.java
+++ src/main/java/gendev/it/serenity/facturation/controller/BillingController.java
@@ -1,7 +1,10 @@
 package gendev.it.serenity.facturation.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,15 +21,29 @@ import gendev.it.serenity.facturation.dto.BillingDTO;
 @RestController
 @RequestMapping("bill")
 @CrossOrigin(methods = { RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT })
-public class BillingController extends CommonController<BillingDTO, BillingService>{
+public class BillingController extends CommonController<BillingDTO, BillingService> {
 
     public BillingController(BillingService service) {
         super(service);
-        //TODO Auto-generated constructor stub
+        // TODO Auto-generated constructor stub
     }
+
     @PutMapping("/update/{id}")
-    public ResponseEntity<?> deleteById(@PathVariable String id, @RequestBody BillingDTO toUpdate, @RequestParam Boolean isDetail) throws BusinessException {
-            return ResponseEntity.ok(getService().updateBilling(toUpdate, id, isDetail));
+    public ResponseEntity<?> deleteById(@PathVariable String id, @RequestBody BillingDTO toUpdate,
+            @RequestParam Boolean isDetail) throws BusinessException {
+        return ResponseEntity.ok(getService().updateBilling(toUpdate, id, isDetail));
     }
-    
+
+    @GetMapping("/customers")
+    public ResponseEntity<?> findAllCustomerBilled() {
+        return null;
+    }
+
+    @GetMapping("/customers/{customerid}")
+    public ResponseEntity<?> findCustomersBilled(@PathVariable String customerid,
+            @RequestParam(required = false, name = "start") LocalDate start,
+            @RequestParam(required = false, name = "end") LocalDate end) {
+        return null;
+    }
+
 }
